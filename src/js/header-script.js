1e24310375eefd2b8c1f026b2ce3f1292fc8a2e6
Fetisova Tetiana <tbabich3003@gmail.com>
2019-12-27 15:06:04 +0200

    menu bug fixed

--- src/js/header-script.js
+++ src/js/header-script.js
@@ -4,7 +4,8 @@ burgerMenuBtn.addEventListener('click', ()=>{
   burgerMenu.classList.add('active');
 });
 document.body.addEventListener('click',(e)=>{
-  if(!e.target.classList.contains('header-menu__burger-menu') || !e.target.classList.contains('header-menu__burger-btn') ){
+  console.log(e.target);
+  if(!e.target.classList.contains('header-menu__burger-btn') && !e.target.classList.contains('header-menu__burger-btn-letters')){
     if(burgerMenu.classList.contains('active')){
       burgerMenu.classList.remove('active')
     }
