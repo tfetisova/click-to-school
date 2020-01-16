d3082daa13d602439abc1081dc5584996337b803
Fetisova Tetiana <tbabich3003@gmail.com>
2020-01-16 13:56:20 +0200

    map settings

--- src/js/header-script.js
+++ src/js/header-script.js
@@ -4,7 +4,6 @@ burgerMenuBtn.addEventListener('click', ()=>{
   burgerMenu.classList.add('active');
 });
 document.body.addEventListener('click',(e)=>{
-  console.log(e.target);
   if(!e.target.classList.contains('header-menu__burger-btn') && !e.target.classList.contains('header-menu__burger-btn-letters')){
     if(burgerMenu.classList.contains('active')){
       burgerMenu.classList.remove('active')
