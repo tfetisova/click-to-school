f0441fa9a72ae3bc008622e2105b3af4dc689e92
Fetisova Tetiana <tbabich3003@gmail.com>
2020-01-21 17:17:40 +0200

    anthem section and news slider added

--- src/js/news-slider.js
+++ src/js/news-slider.js
@@ -1,17 +1,3 @@
-const burgerMenuBtn = document.querySelector('.header-menu__burger-btn');
-const burgerMenu = document.querySelector('.header-menu__burger-menu');
-burgerMenuBtn.addEventListener('click', ()=>{
-  burgerMenu.classList.add('active');
-});
-document.body.addEventListener('click',(e)=>{
-  if(!e.target.classList.contains('header-menu__burger-btn') && !e.target.classList.contains('header-menu__burger-btn-letters')){
-    if(burgerMenu.classList.contains('active')){
-      burgerMenu.classList.remove('active')
-    }
-  }
-});
-
-
 function sliderBuild(activeSlide) {
   activeSlide.previousElementSibling.classList.add('near-active');
   activeSlide.nextElementSibling.classList.add('near-active');
